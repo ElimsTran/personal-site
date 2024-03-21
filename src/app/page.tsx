2f23c27078f76e3070b6dbe89996c096f1a84d6f
longbinkg@gmail.com <longbinkg@gmail.com>
2024-03-21 07:46:49 +0700

    add: remaining time - Count down until toiec test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useEffect, useMemo, useState } from "react";
+import { Box, CircularProgress, LinearProgress } from "@mui/material";
+import { useLayoutEffect, useMemo, useState } from "react";
 
 const DUE_DATE = "2024-04-14 10:30:00";
 
@@ -7,14 +8,16 @@ export default function Home() {
   const dueDay: Date = useMemo(() => new Date(DUE_DATE), []);
   const [timer, setTimer] = useState<string>("");
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const countInterval = setInterval(() => {
       const diffTime = dueDay.getTime() - Date.now();
       const seconds = Math.floor((diffTime / 1000) % 60);
       const minutes = Math.floor((diffTime / 1000 / 60) % 60);
       const hours = Math.floor((diffTime / (1000 * 60 * 60)) % 24);
       const days = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-      setTimer(`${days}D:${hours}H:${minutes}M:${seconds}S`);
+      setTimer(
+        `${days} Days ${hours} hours ${minutes} minutes ${seconds} seconds`
+      );
     }, 1000);
 
     return () => {
@@ -23,8 +26,9 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 justify-center align-middle font-bold">
-      REMAINING TIME TO TOEIC TEST <br />
+    <main className="flex min-h-screen flex-col items-center p-24 justify-center align-middle font-bold gap-1 text-[24px]">
+      REMAINING TIME UNTIL TOEIC TEST
+      {!timer && <CircularProgress color="primary" />}
       <span className="flex justify-center">{timer}</span>
     </main>
   );
