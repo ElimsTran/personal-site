b78b49490711dd012f2dc62cdd24730bea1d21c2
longbinkg@gmail.com <longbinkg@gmail.com>
2024-04-25 17:32:36 +0700

    Update: add shadow for message box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,11 @@ export default function Home() {
   return (
     <Container component={"main"} maxWidth="md" className="flex flex-col">
       <Box className="flex flex-col items-center justify-center align-middle gap-2 select-none min-h-[84vh] ">
-        <Box className="p-5 rounded-md text-justify triangle-left bg-elims-backgroundColorLight w-auto max-w-[370px]">
+        <Box className="p-5 rounded-md text-justify triangle-left bg-elims-backgroundColorLight w-auto max-w-[370px] shadow-lg shadow-elims-backgroundColorDark">
           {INTRODUCE_SENTENCE}
         </Box>
         <Box className="flex flex-col justify-center items-center gap-5">
-          <Box className="w-[150px] h-[150px] rounded-[50%] flex items-center justify-center overflow-hidden shadow-sm reveal-animation bg-[url('/pixelRoom.jpg')] bg-fixed bg-contain">
+          <Box className="w-[150px] h-[150px] rounded-[50%] flex items-center justify-center overflow-hidden shadow-lg shadow-elims-backgroundColorDark reveal-animation bg-[url('/pixelRoom.jpg')] bg-fixed bg-contain">
             <Image
               src={"/avt_hi.webp"}
               alt="avt"
--- src/components/ultilities/SpotifyPlaylist.tsx
+++ src/components/ultilities/SpotifyPlaylist.tsx
@@ -8,7 +8,7 @@ type SportifyPlaylistProps = {
 
 export default function SpotifyPlaylist(props: SportifyPlaylistProps) {
   return (
-    <Box className="max-w-[350px] opacity-50 fixed top-[3%] right-0 translate-x-[calc(100%)] border-[1px] [&:hover]:translate-x-0 [&:hover]:opacity-100 transition ease-in-out duration-500 z-50 delay-100">
+    <Box className="max-w-[350px] opacity-50 fixed top-[3%] right-0 translate-x-[calc(100%)] border-[1px] [&:hover]:translate-x-0 [&:hover]:opacity-100 transition ease-in-out duration-500 z-50 delay-200">
       <Box className="translate-x-[-61px] translate-y-[87px] -rotate-90 uppercase text-[12px] font-bold bg-[#203040] border-solid border-[2px] border-b-0 w-[80px] h-[40px] rounded-t-md ">
         <Image
           src={"/logos/spotify.svg"}
@@ -17,7 +17,10 @@ export default function SpotifyPlaylist(props: SportifyPlaylistProps) {
           layout="fill"
         />
       </Box>
-      <Typography component={"text"} className="w-full flex justify-center">
+      <Typography
+        component={"text"}
+        className="w-full flex justify-center select-none"
+      >
         I hope you like that
       </Typography>
       <Box className="border-solid border-[2px] rounded-b-md bg-[#203040] p-0 m-0">
