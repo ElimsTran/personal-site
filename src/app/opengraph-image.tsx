44141247cde6397a5838da6ed6c3688f7741eac4
Trần Phi Long <50954310+ElimsTran@users.noreply.github.com>
2024-05-28 21:55:21 +0700

    Update opengraph-image.tsx

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,21 +1,6 @@
 import { ImageResponse } from "next/og";
 import Img from "next/image";
-// Route segment config
-export const runtime = "edge";
-
-// Image metadata
-export const alt = "About Acme";
-export const size = {
-  width: 1200,
-  height: 630,
-};
-
-export const contentType = "image/png";
-
-// Image generation
 export default async function Image() {
-  // Font
-
   return new ImageResponse(
     (
       // ImageResponse JSX element
@@ -23,15 +8,9 @@ export default async function Image() {
         src={"/Elims.jpg"}
         alt="preImg"
         loading={"eager"}
-        width={150}
+        width={300}
         height={150}
       />
-    ),
-    // ImageResponse options
-    {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
-      ...size,
-    }
+    )
   );
 }
