7fd524f81d8c186b18e45edd5d67ff08e75d0118
Trần Phi Long <50954310+ElimsTran@users.noreply.github.com>
2024-05-28 22:03:52 +0700

    Update opengraph-image.tsx

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,16 +1,48 @@
 import { ImageResponse } from "next/og";
-import Img from "next/image";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const alt = "Elims's Zone";
+export const size = {
+  width: 200,
+  height: 100,
+};
+
+export const contentType = "image/png";
+
+// Image generation
 export default async function Image() {
+  // Font
+
   return new ImageResponse(
     (
       // ImageResponse JSX element
-      <Img
-        src={"/Elims.jpg"}
-        alt="preImg"
-        loading={"eager"}
-        width={300}
-        height={150}
-      />
-    )
+      <div
+        style={{
+          fontSize: 24,
+          background: "#2f3045",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "#fdbf08",
+          fontWeight: "900",
+          border: "solid",
+          borderRadius: "5px",
+          borderColor: "#2f3045",
+        }}
+      >
+        Elims&apos;s Zone
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+    }
   );
 }
